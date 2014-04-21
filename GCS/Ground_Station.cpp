8dda7e3efe5022f8daafd2982736251ee93dd56b
Edward <huangxx_2155@qq.com>
2014-04-21 19:11:33 +0800

    for sync

--- GCS/Ground_Station.cpp
+++ GCS/Ground_Station.cpp
@@ -1,3 +1,12 @@
+/*
+
+        This source code is written by Edward HUANG, and Tony Yi
+        For the development of their Final Year Project
+        Department of Computer Science and Engineering
+        The Chinese University of HONG KONG
+
+*/
+        
 #include "Ground_Station.h"
 
 #define NORTH 0
